7aafb460d0a709e65e30cc9357a3b807afcf0452
Philip Sjöberg <philip.sjoberg@avinode.com>
2025-04-07 07:23:38 +0200

    Update argo workflow

---
+++ utils/argo_workflow_ui.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+echo "Port forwarding Argo Workflow UI: https://localhost:2746/"
+kubectl port-forward deployment/argo-server -n argo-workflow 2746:2746 > /dev/null &
+open https://localhost:2746/
